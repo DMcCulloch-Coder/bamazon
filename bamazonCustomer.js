999632555e555edc64b5b4b656ae7b0e2b344160
David <DMcCulloch86@gmail.com>
2019-12-03 16:34:05 -0500

    created bamazon database and connected it to customer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,21 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+})
 
+connection.connect(function(err) {
+    if (err) console.log(err)
+    console.log(`Connected as id ${connection.threadId}`)
+    startApp();
+})
+
+function startApp() {
+    console.log('start')
+    connection.end()
+}
